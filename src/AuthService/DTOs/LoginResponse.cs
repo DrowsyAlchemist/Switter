b1955204c84f446b7b3d6d3b1bc50f11058205fe
DrowsyAlchemist <michaelGulidow@yandex.ru>
2025-09-26 22:12:32 +0300

    Implement LoginResponse DTO

--- src/AuthService/DTOs/LoginResponse.cs
+++ src/AuthService/DTOs/LoginResponse.cs
@@ -2,5 +2,11 @@
 {
     public class LoginResponse
     {
+        public Guid UserId { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string AccessToken { get; set; } = string.Empty;
+        public string RefreshToken { get; set; } = string.Empty;
+        public DateTime ExpiresAt { get; set; }
     }
 }
