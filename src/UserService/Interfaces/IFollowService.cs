8319808dbc7d3e46fe1d399a53cdec7ff6ffdbee
DrowsyAlchemist <michaelGulidow@yandex.ru>
2025-11-05 15:13:36 +0300

    feat: Add interfaces for services (Profile, Follow)

---
+++ src/UserService/Interfaces/IFollowService.cs
@@ -0,0 +1,13 @@
+﻿using UserService.DTOs;
+
+namespace UserService.Interfaces
+{
+    public interface IFollowService
+    {
+        Task<bool> FollowUserAsync(Guid followerId, Guid followeeId);
+        Task<bool> UnfollowUserAsync(Guid followerId, Guid followeeId);
+        Task<List<UserProfileDto>> GetFollowersAsync(Guid userId, int page = 1, int pageSize = 20);
+        Task<List<UserProfileDto>> GetFollowingAsync(Guid userId, int page = 1, int pageSize = 20);
+        Task<bool> IsFollowingAsync(Guid followerId, Guid followeeId);
+    }
+}
---
+++ src/UserService/Interfaces/IUserProfileService.cs
@@ -0,0 +1,11 @@
+﻿using UserService.DTOs;
+
+namespace UserService.Interfaces
+{
+    public interface IUserProfileService
+    {
+        Task<UserProfileDto?> GetProfileAsync(Guid userId, Guid? currentUserId = null);
+        Task<UserProfileDto> UpdateProfileAsync(Guid userId, UpdateProfileRequest request);
+        Task<List<UserProfileDto>> SearchUsersAsync(string query, int page = 1, int pageSize = 20);
+    }
+}
