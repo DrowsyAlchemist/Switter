bf01719b10c15c315a0036ebedc5142137a3a7a8
DrowsyAlchemist <michaelGulidow@yandex.ru>
2025-11-26 10:27:32 +0300

    feat(Repos): Add exceptions, use AsNoTracking()

--- src/UserService/Data/ProfilesRepository.cs
+++ src/UserService/Data/ProfilesRepository.cs
@@ -7,43 +7,78 @@ namespace UserService.Data
     public class ProfilesRepository : IProfilesRepository
     {
         private readonly UserDbContext _context;
+        private readonly ILogger<ProfilesRepository> _logger;
 
-        public ProfilesRepository(UserDbContext context)
+        public ProfilesRepository(UserDbContext context, ILogger<ProfilesRepository> logger)
         {
             _context = context;
+            _logger = logger;
         }
 
         public async Task<List<UserProfile>> GetUsersAsync()
         {
-            return await _context.Profiles.AsNoTracking().ToListAsync();
+            try
+            {
+                return await _context.Profiles.AsNoTracking().ToListAsync();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Db is unavailable");
+                throw new Exception("Db is unavailable", ex);
+            }
         }
 
         public async Task<UserProfile?> GetProfileAsync(Guid userId)
         {
-            return await _context.Profiles
-                .Where(p => p.Id == userId)
-                .AsNoTracking()
-                .FirstOrDefaultAsync();
+            try
+            {
+                return await _context.Profiles
+                    .Where(p => p.Id == userId)
+                    .AsNoTracking()
+                    .FirstOrDefaultAsync();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Db is unavailable");
+                throw new Exception("Db is unavailable", ex);
+            }
         }
 
         public async Task<UserProfile> AddAsync(UserProfile profile)
         {
             ArgumentNullException.ThrowIfNull(profile);
 
-            bool isExist = await _context.Profiles.AnyAsync(p => p.Id.Equals(profile.Id));
-            if (isExist)
-                throw new InvalidOperationException("Profile with this id already exists.");
+            try
+            {
+                bool isExist = await _context.Profiles.AnyAsync(p => p.Id.Equals(profile.Id));
+                if (isExist)
+                    throw new InvalidOperationException("Profile with this id already exists.");
 
-            await _context.Profiles.AddAsync(profile);
-            return profile;
+                await _context.Profiles.AddAsync(profile);
+                return profile;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Db is unavailable");
+                throw new Exception("Db is unavailable", ex);
+            }
         }
 
         public async Task<UserProfile> UpdateProfileAsync(UserProfile profile)
         {
             ArgumentNullException.ThrowIfNull(profile);
-            _context.Profiles.Update(profile);
-            await _context.SaveChangesAsync();
-            return profile;
+
+            try
+            {
+                _context.Profiles.Update(profile);
+                await _context.SaveChangesAsync();
+                return profile;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Db is unavailable");
+                throw new Exception("Db is unavailable", ex);
+            }
         }
     }
 }
